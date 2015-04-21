bc65784ef8a44a5ed45882fa913ed1b5f554dddf
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2015-04-21 09:00:31 +0200

    Make sure that we received all status message parameters before we initialize it by using a timer with a 500 ms delay.

--- lua/shine/extensions/pregameplus/client.lua
+++ lua/shine/extensions/pregameplus/client.lua
@@ -36,18 +36,24 @@ end
 function Plugin:ShowStatus( NewStatus )
 	if NewStatus then
 		if not self.Status then
-			self.Status = Shine:AddMessageToQueue( 70, self.dt.StatusX, self.dt.StatusY, self.dt.StatusText, 1,
-				self.dt.StatusR, self.dt.StatusG, self.dt.StatusB, 0, 1,0 )
-			self.Status.UpdateText = function(TextObject)
-				if self.dt.Countdown then
-					local Text = string.gsub( TextObject.Text, "<t>",
-						string.TimeToString( TextObject.Duration ) )
-					TextObject.Obj:SetText( Text )
-				end
+			--We use a timer here as the database fields don't ge networked the same way we intialized them at the
+			--server-side. This means the status can change even before the message's position was networked.
+			if not self:GetTimer( "StatusSetup" )then
+				self:CreateTimer( "StatusSetup", 0.5, 1, function()
+					self.Status = Shine:AddMessageToQueue( 70, self.dt.StatusX, self.dt.StatusY, self.dt.StatusText, 1,
+						self.dt.StatusR, self.dt.StatusG, self.dt.StatusB, 0, 1,0 )
+					self.Status.UpdateText = function(TextObject)
+						if self.dt.Countdown then
+							local Text = string.gsub( TextObject.Text, "<t>",
+								string.TimeToString( TextObject.Duration ) )
+							TextObject.Obj:SetText( Text )
+						end
 
-				if TextObject.Duration == 0 then
-					TextObject.Duration = 1
-				end
+						if TextObject.Duration == 0 then
+							TextObject.Duration = 1
+						end
+					end
+				end)
 			end
 		else
 			self.Status.Obj:SetIsVisible(true)
@@ -60,11 +66,11 @@ end
 function Plugin:UpdateStatusText( NewText )
 	if not self.Status then
 		self:ShowStatus(true)
-	end
-
-	self.Status.Text = NewText
-	if not self.dt.Countdown then
-		self.Status.Obj:SetText( NewText )
+	else
+		self.Status.Text = NewText
+		if not self.dt.Countdown then
+			self.Status.Obj:SetText( NewText )
+		end
 	end
 end
 
@@ -76,6 +82,7 @@ end
 
 function Plugin:Cleanup()
 	Shine:RemoveMessage(70)
+	self.Status = nil
 
 	self.BaseClass.Cleanup( self )
 
