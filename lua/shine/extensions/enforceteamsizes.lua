129e8084b84938768c226272b934d99bd1aadf46
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2015-04-26 12:15:30 +0200

    Made the enforceteamsize plug-in's config easier to set up.

--- lua/shine/extensions/enforceteamsizes.lua
+++ lua/shine/extensions/enforceteamsizes.lua
@@ -3,30 +3,28 @@
 --Original source can be found at https://github.com/akrigline/EnforceTeamSize
 ]]
 local Shine = Shine
-
 local Plugin = {}
 
 Plugin.HasConfig = true
-Plugin.ConfigName = "EnforceTeamsizes.json"
+Plugin.ConfigName = "EnforceTeamSizes.json"
 
 --[[
 --TeamNumbers:
- - 0: RR
  - 1: Marines
  - 2: Aliens
  - 3: Spec
  ]]
 Plugin.DefaultConfig = {
     Teams = {
-        [1] = {
+        Team1 = {
             MaxPlayers = 8,
-            TooManyMessage = "The %s have currently too many players. Please Spectate until the round ends.",
+            TooManyMessage = "The %s have currently too many players. Please spectate until the round ends.",
             InformAboutFreeSpace = {3},
             InformMessage = "A player left the %s. So you can join up now."
         },
-        [2] = {
+        Team2 = {
             MaxPlayers = 8,
-            TooManyMessage = "The %s have currently too many players. Please Spectate until the round ends.",
+            TooManyMessage = "The %s have currently too many players. Please spectate until the round ends.",
             InformAboutFreeSpace = {3},
             InformMessage = "A player left the %s. So you can join up now."
         }
@@ -43,7 +41,7 @@ end
 
 function Plugin:Notify(Player, Message, OldTeam)
     Shine:NotifyDualColour( Player, self.Config.MessageNameColor[1], self.Config.MessageNameColor[2],
-        self.Config.MessageNameColor[3], "EnforcedTeamSizes", 255, 255, 255,
+        self.Config.MessageNameColor[3], "[EnforcedTeamSizes]", 255, 255, 255,
        Message, true, Shine:GetTeamName(OldTeam, true) )
 end
 
@@ -57,13 +55,14 @@ end
 function Plugin:PostJoinTeam( Gamerules, _, OldTeam )
     if OldTeam < 0 then return end
 
-    if self.Config.Teams[OldTeam] and #self.Config.Teams[OldTeam].InformAboutFreeSpace ~= 0 and
-            Gamerules:GetTeam(OldTeam):GetNumPlayers() + 1 == self.Config.Teams[OldTeam].MaxPlayers then
-        for _, i in ipairs(self.Config.Teams[OldTeam].InformAboutFreeSpace) do
+    local TeamIndex = string.format("Team%s", OldTeam)
+    if self.Config.Teams[TeamIndex] and #self.Config.Teams[TeamIndex].InformAboutFreeSpace ~= 0 and
+            Gamerules:GetTeam(OldTeam):GetNumPlayers() + 1 == self.Config.Teams[TeamIndex].MaxPlayers then
+        for _, i in ipairs(self.Config.Teams[TeamIndex].InformAboutFreeSpace) do
             local Team = Gamerules:GetTeam(i)
             local Players = Team and Team:GetPlayers()
             if Players and #Players ~= 0 then
-                self:Notify(Players, self.Config.Teams[OldTeam].InformMessage
+                self:Notify(Players, self.Config.Teams[TeamIndex].InformMessage
                         or "A player left the %s team. So you can join up now.", OldTeam)
             end
         end
@@ -71,12 +70,13 @@ function Plugin:PostJoinTeam( Gamerules, _, OldTeam )
 end
 
 function Plugin:JoinTeam( Gamerules, Player, NewTeam, _, ShineForce )
-    if ShineForce or not self.Config.Teams[NewTeam] then return end
+	local TeamIndex = string.format("Team%s", NewTeam)
+    if ShineForce or NewTeam == 0 or not self.Config.Teams[TeamIndex] then return end
 
     --Check if team is above MaxPlayers
-    if Gamerules:GetTeam(NewTeam):GetNumPlayers() >= self.Config.Teams[NewTeam].MaxPlayers then
+    if Gamerules:GetTeam(NewTeam):GetNumPlayers() >= self.Config.Teams[TeamIndex].MaxPlayers then
         --Inform player
-        self:Notify(Player, self.Config.Teams[NewTeam].TooManyMessage, NewTeam)
+        self:Notify(Player, self.Config.Teams[TeamIndex].TooManyMessage, NewTeam)
         return false
     end
 end
