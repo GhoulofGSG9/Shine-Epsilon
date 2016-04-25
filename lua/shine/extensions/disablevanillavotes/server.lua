1e491c449db89d9e0d8c480d009c001e045afa60
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2016-04-25 07:33:02 +0200

    Added a option to disablevanillavotes to disable a vote if admins are at the server.

--- lua/shine/extensions/disablevanillavotes/server.lua
+++ lua/shine/extensions/disablevanillavotes/server.lua
@@ -14,22 +14,27 @@ Plugin.ConfigName = "DisableVanillaVotes.json"
 Plugin.DefaultConfig = {
 	VoteChangeMap = {
 		Enabled = true,
+		DisableWhenAdminOnline = false,
 		Message = "To vote to change the map, press %s > RTV."
 	},
 	VotingForceEvenTeams = {
 		Enabled = true,
+		DisableWhenAdminOnline = false,
 		Message = "To vote to force even teams, press %s > Shuffle."
 	},
 	VoteRandomizeRR = {
 		Enabled = true,
+		DisableWhenAdminOnline = false,
 		Message = "To vote to randomize teams, press %s > Shuffle."
 	},
 	VoteKickPlayer = {
 		Enabled = true,
+		DisableWhenAdminOnline = true,
 		Message = "Please contact a server operator to get a player kicked."
 	},
 	VoteResetGame = {
 		Enabled = true,
+		DisableWhenAdminOnline = true,
 		Message = "Please contact a  server operator to reset the game."
 	},
 	Message = {
@@ -43,6 +48,7 @@ Plugin.CheckConfigTypes = true
 
 function Plugin:Initialise()
 	self.Enabled = true
+	self.AdminClients = {}
 
 	self.dt.R = math.Clamp(self.Config.Message.R, 0, 255)
 	self.dt.G = math.Clamp(self.Config.Message.G, 0, 255)
@@ -51,13 +57,29 @@ function Plugin:Initialise()
 	return true
 end
 
+function Plugin:ClientConfirmConnect(Client)
+	if Shine:HasAccess( Client, "sh_disablevotes" ) then
+		table.insert(self.AdminClients, Client:GetUserId())
+	end
+end
+
+function Plugin:ClientDisconnect(Client)
+	local id = Client:GetUserId()
+
+	for i = #self.AdminClients, 1, -1 do
+		if self.AdminClients[i] == id then
+			table.remove(self.AdminClients, i)
+			return
+		end
+	end
+end
+
 function Plugin:NS2StartVote(VoteName, Client)
 	local VoteOption =  self.Config[VoteName]
-	if VoteOption and not VoteOption.Enabled then
+	if VoteOption and (not VoteOption.Enabled or VoteOption.DisableWhenAdminOnline and #self.AdminClients > 0) then
 		self:Notify(Client, VoteOption.Message)
 		return false
 	end
-
 end
 
 function Plugin:Notify( Client, Message )
