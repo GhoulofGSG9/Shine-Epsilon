b2339867f8f2f49eaefc53a5342097adc8f7db41
Sebastian Schuck <sebastian@naturalselection2.com>
2017-11-09 10:45:05 +0100

    Improved the custom spawns plugin: Fixed that it didn't work on certain maps like caged. Fixed that it would effect the spawn setup even when disabled at maps like refinery Added default config files for all vanilla maps.

--- lua/shine/extensions/customspawns.lua
+++ lua/shine/extensions/customspawns.lua
@@ -12,7 +12,7 @@ local IsType = Shine.IsType
 
 local Plugin = {}
 
-Plugin.Version = "1.0"
+Plugin.Version = "1.1"
 Plugin.NS2Only = true
 
 Plugin.HasConfig = true
@@ -21,13 +21,17 @@ Plugin.DefaultConfig =
 {
     Maps = {
         [ "ns2_biodome" ] = true,
+	    [ "ns2_caged" ] = false,
+	    [ "ns2_derelict"] = false,
         [ "ns2_descent" ] = true,
         [ "ns2_docking" ] = true,
+	    [ "ns2_eclipse" ] = true,
+	    [ "ns2_kodiak" ] = false,
         [ "ns2_mineshaft" ] = true,
+        [ "ns2_refinery" ] = false,
         [ "ns2_summit" ] = true,
-        [ "ns2_eclipse" ] = true,
+        [ "ns2_tram" ] = false,
         [ "ns2_veil" ] = false,
-        [ "ns2_kodiak" ] = false
     }
 }
 
@@ -43,7 +47,56 @@ local MapConfigs = {
 		},
 		{
 			name = "Atmosphere Exchange",
+			team = "aliens",
+			enemyspawns = {
+				"Reception"
+			}
+		}
+	},
+	[ "ns2_caged" ] = {
+		{
+			name = "main hold",
+			team = "marines",
+			enemyspawns = {
+				"ventilation system"
+			}
+		},
+		{
+			name = "ventilation system",
+			team = "aliens",
+			enemyspawns = {
+				"main hold"
+			}
+		}
+	},
+	[ "ns2_derelict" ] = {
+		{
+			enemyspawns = {
+				"geothermal"
+			},
+			name = "western entrance",
+			team = "marines"
+		},
+		{
+			enemyspawns = {
+				"atmospheric seeding"
+			},
+			name = "garage",
+			team = "both"
+		},
+		{
+			enemyspawns = {
+				"western entrance"
+			},
+			name = "geothermal",
 			team = "aliens"
+		},
+		{
+			enemyspawns = {
+				"garage"
+			},
+			name = "atmospheric seeding",
+			team = "both"
 		}
 	},
 	[ "ns2_descent" ] = {
@@ -89,6 +142,32 @@ local MapConfigs = {
 			team = "aliens"
 		}
 	},
+	[ "ns2_eclipse" ] = {
+		{
+			name = "Marine Start",
+			team = "marines",
+			enemyspawns = {
+				"Computer Core"
+			}
+		},
+		{
+			name = "Computer Core",
+			team = "aliens"
+		}
+	},
+	[ "ns2_kodiak" ] = {
+		{
+			name = "Asteroid Tracking",
+			team = "marines",
+			enemyspawns = {
+				"Command"
+			}
+		},
+		{
+			name = "Command",
+			team = "aliens"
+		}
+	},
 	[ "ns2_mineshaft" ] = {
 		{
 			name = "Operations",
@@ -111,6 +190,40 @@ local MapConfigs = {
 		{
 			name = "Sorting",
 			team = "aliens"
+		}
+	},
+	[ "ns2_refinery" ] = {
+		{
+			enemyspawns = {
+				"pipeworks"
+			},
+			name = "smelting",
+			team = "both"
+		},{
+			enemyspawns = {
+				"flow control"
+			},
+			name = "containment",
+			team = "both"
+		},{
+			enemyspawns = {
+				"turbine",
+				"smelting"
+			},
+			name = "pipeworks",
+			team = "both"
+		},{
+			enemyspawns = {
+				"containment"
+			},
+			name = "flow control",
+			team = "both"
+		},{
+			enemyspawns = {
+				"pipeworks"
+			},
+			name = "turbine",
+			team = "both"
 		}
 	},
 	[ "ns2_summit" ] = {
@@ -143,17 +256,34 @@ local MapConfigs = {
 			}
 		}
 	},
-	[ "ns2_eclipse" ] = {
+	[ "ns2_tram" ] = {
 		{
-			name = "Marine Start",
-			team = "marines",
-			enemyspawns = { 
-				"Computer Core" 
-			}
-		},
-		{
-			name = "Computer Core",
-			team = "aliens"
+			enemyspawns = {
+				"shipping"
+			},
+			name = "warehouse",
+			team = "both"
+		},{
+			enemyspawns = {},
+			name = "repair room",
+			team = "none"
+		},{
+			enemyspawns = {
+				"shipping"
+			},
+			name = "server room",
+			team = "both"
+		},{
+			enemyspawns = {
+				"warehouse",
+				"server room"
+			},
+			name = "shipping",
+			team = "both"
+		},{
+			enemyspawns = {},
+			name = "elevator transfer",
+			team = "none"
 		}
 	},
 	[ "ns2_veil" ] = {
@@ -166,19 +296,6 @@ local MapConfigs = {
 		},
 		{
 			name = "Cargo",
-			team = "aliens"
-		}
-	},
-	[ "ns2_kodiak" ] = {
-		{
-			name = "Asteroid Tracking",
-			team = "marines",
-			enemyspawns = { 
-				"Command" 
-			}
-		},
-		{
-			name = "Command",
 			team = "aliens"
 		}
 	},
@@ -195,9 +312,8 @@ SetupClassHook( "NS2Gamerules", "ChooseTechPoint", "OverrideChooseTechPoint",
     return TechPoint
 end )
 SetupClassHook( "NS2Gamerules", "ResetGame", "OnGameReset", "PassivePre")
-SetupClassHook( "TechPoint", "OnInitialized", "TechPointIntialized", "PassivePost")
-SetupClassHook( "TechPoint", "GetChooseWeight", "OnGetChooseWeight", "ActivePre")
-SetupClassHook( "TechPoint", "GetTeamNumberAllowed", "OnGetTeamNumberAllowed", "ActivePre")
+SetupClassHook( "TechPoint", "GetChooseWeight", "OnTechPointGetChooseWeight", "ActivePre")
+SetupClassHook( "TechPoint", "GetTeamNumberAllowed", "OnTechPointGetTeamNumberAllowed", "ActivePre")
 SetupClassHook( "AlienTeam", "SpawnInitialStructures", "PostAlienTeamSpawnInitialStructures",
 	function( OldFunc, self, TechPoint)
 		local Tower, CommandStructure = OldFunc(self, TechPoint)
@@ -211,7 +327,6 @@ function Plugin:Initialise()
 		return false, StringFormat( "The customspawns plugin does not work with %s.", self.Gamemode )
 	end
 
-	self.TechPoints = {}
 	self.Enabled = true
 	
     return true
@@ -237,35 +352,34 @@ local function LoadMapConfig( Mapname, Gamemode )
 	return MapConfig
 end
 
-function Plugin:TechPointIntialized( TechPoint )
-	--Don't rebuild table after we have parsed the config!
-	if self.Spawns then return end
-
-	local name = Lower(TechPoint:GetLocationName())
-	if name == "" then return end --seems like there are sometimes techpoints without a name
-
-	self.TechPoints[ name ] = TechPoint
-end
-
-function Plugin:OnGetChooseWeight()
+function Plugin:OnTechPointGetChooseWeight()
 	return 1
 end
 
 function Plugin:ParseMapConfig()
 	local MapName = Lower( Shared.GetMapName() )
+
+	self.Spawns = GetEntities("TechPoint")
+	-- convert list into a set
+	for i = 1, #self.Spawns do
+		local TechPoint = self.Spawns[i]
+		local Name = Lower( TechPoint:GetLocationName() )
+
+		self.Spawns[Name] = TechPoint
+	end
+
 	if not self.Config.Maps[ MapName ] then
 		self.Enabled = false --So the command might be used
 		return
 	end
-
-	self.Spawns = self.TechPoints
 
 	local Spawns = LoadMapConfig( MapName, self.Gamemode )
 
 	local NumAlienSpawns = 0
 	local NumMarineSpawns = 0
 
-	for _, Spawn in ipairs(Spawns) do
+	for i = 1, #Spawns do
+		local Spawn = Spawns[i]
 		local name = Lower( Spawn.name )
 
 		if not self.Spawns[ name ] then
@@ -304,22 +418,8 @@ function Plugin:ParseMapConfig()
 	end
 end
 
-function Plugin:OnGetTeamNumberAllowed( TechPoint )
-	if self.Spawns == nil then
-		local error = self:ParseMapConfig()
-		if error then
-			Shared.Message(StringFormat("[CustomSpawns] Error, %s", error))
-			Shared.Message("[CustomSpawns] Unloading the plugin now ...")
-			Shine:UnloadExtension( "customspawns" )
-		else
-			--doing this here as map has been completly loaded at this point.
-			self:CreateCommands()
-		end
-	end
-
-	if not self.Enabled then return end
-
-	local name =  Lower( TechPoint:GetLocationName() )
+function Plugin:OnTechPointGetTeamNumberAllowed( TechPoint )
+	local name = Lower( TechPoint:GetLocationName() )
 	return self.Spawns[ name ] and self.Spawns[ name ].team or 3
 end
 
@@ -345,11 +445,26 @@ function Plugin:PreChooseTechPoint( _, _, TeamNumber)
 end
 
 function Plugin:OnGameReset()
+	if self.Spawns == nil then
+		local error = self:ParseMapConfig()
+		if error then
+			Shared.Message(StringFormat("[CustomSpawns] Error, %s", error))
+			Shared.Message("[CustomSpawns] Unloading the plugin now ...")
+			Shine:UnloadExtension( "customspawns" )
+		else
+			--doing this here as map has been completly loaded at this point.
+			self:CreateCommands()
+		end
+	end
+
+	if not self.Enabled then return end
+
     Server.spawnSelectionOverrides = false
     Server.teamSpawnOverride = false
 end
 
---Needed to avoid that Harvester die at tech point where no cysts are pre-placed
+-- Needed to avoid that Harvester die at tech point where no cysts are pre-placed
+-- Todo: Merge this into vanilla
 function Plugin:PostAlienTeamSpawnInitialStructures( Team, _, Tower )
 	local origin = Tower:GetOrigin()
 	if #GetEntitiesWithinRange( "Cyst", origin, kInfestationRadius ) > 0 then return end
@@ -396,15 +511,31 @@ function Plugin:CreateCommands()
 			[3] = "none",
 		}
 
-		for name, Spawn in pairs(self.TechPoints) do
-			table.insertunique(Spawns[Spawn.allowedTeamNumber], name )
+		local TechPoints = GetEntities("TechPoint")
+		for i = 1, #TechPoints do
+			local TechPoint = TechPoints[i]
+			local Name = Lower( TechPoint:GetLocationName() )
+			table.insertunique(Spawns[TechPoint.allowedTeamNumber], Name )
 		end
 
-		local function GetEnemySpawns( TeamNumber )
+		local function GetEnemySpawns( TeamNumber, TpName )
 			if TeamNumber > 2 then return {} end
 
-			local function JoinTables( t1, t2 )
-				for _, name in ipairs( t2 ) do
+			local function filter(t, v)
+				for i = 1, #t do
+					local e = t[i]
+					if e == v then
+						table.remove(t, i)
+						break
+					end
+				end
+
+				return t
+			end
+
+			local function merge( t1, t2 )
+				for i = 1, #t2 do
+					local name = t2[i]
 					t1[#t1 + 1] = name
 				end
 				return t1
@@ -414,23 +545,26 @@ function Plugin:CreateCommands()
 			local spawns = table.Copy(Spawns[0]) --localize, so we don't overriding the original table
 
 			if TeamNumber < 2 then
-				return JoinTables( spawns, Spawns[2] )
+				return filter(merge( spawns, Spawns[2] ), TpName)
 			else
-				return JoinTables( spawns, Spawns[1] )
+				return filter(merge( spawns, Spawns[1] ), TpName)
 			end
 		end
 
-		for name, Spawn in pairs(self.TechPoints) do
+		for i = 1, #TechPoints do
+			local TechPoint = TechPoints[i]
+			local Name = Lower( TechPoint:GetLocationName() )
 			local SpawnEntry = {}
-			SpawnEntry.name = name
-			SpawnEntry.team = TeamNames[ Spawn.allowedTeamNumber ]
-			SpawnEntry.enemyspawns = GetEnemySpawns( Spawn.allowedTeamNumber )
+
+			SpawnEntry.name = Name
+			SpawnEntry.team = TeamNames[ TechPoint.allowedTeamNumber ]
+			SpawnEntry.enemyspawns = GetEnemySpawns( TechPoint.allowedTeamNumber, Name )
 			SpawnsTable[ #SpawnsTable + 1 ] = SpawnEntry
 		end
 
 		local filename = StringFormat( "%scustomspawns/%s.json", Shine.Config.ExtensionDir, DumpName )
-		self:Notify( Client, "Spawn Dump has been saved to %s!", true, filename)
 		Shine.SaveJSONFile( SpawnsTable, filename )
+		self:Notify( Client, "Spawn Dump has been saved to %s!", true, filename)
 	end
 
 	local DumpSpawnsCommand = self:BindCommand( "sh_dumpspawns", "dumpspawns", DumpSpawns )
@@ -439,7 +573,6 @@ function Plugin:CreateCommands()
 end
 
 function Plugin:Cleanup()
-	self.TechPoints = nil
 	self.Spawns = nil
 	self.ValidAlienSpawn = nil
 
