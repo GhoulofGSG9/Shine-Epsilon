d3c7a4609bbff82926eb35a1c58e2b90c1e090d3
Sebastian Schuck <sebastian@naturalselection2.com>
2017-12-04 19:56:02 +0100

    Added option to passwordreset to set a start password

--- lua/shine/extensions/passwordreset.lua
+++ lua/shine/extensions/passwordreset.lua
@@ -14,12 +14,19 @@ Plugin.DefaultConfig =
 {
     MinPlayer = 0,
     ResetTime = 10,
-    DefaultPassword = ""    
+    DefaultPassword = "",
+    StartPassword = ""
 }
 Plugin.CheckConfig = true
 
 function Plugin:Initialise()
     self.Enabled = true
+
+    if self.Config.StartPassword ~= "" then
+        Notify( "[PasswordReset] Set password to start one" )
+        Server.SetPassword(tostring(self.Config.StartPassword))
+    end
+
     return true
 end
 
@@ -29,9 +36,10 @@ function Plugin:ClientConnect()
 end
 
 function Plugin:ClientDisconnect()
-    if Shine.GetHumanPlayerCount() > self.Config.MinPlayer then return end
+    if Shine.GetHumanPlayerCount() >= self.Config.MinPlayer then return end
+
     self:SimpleTimer( self.Config.ResetTime * 60, function()
-	if Shine.GetHumanPlayerCount() > self.Config.MinPlayer then return end
+	if Shine.GetHumanPlayerCount() >= self.Config.MinPlayer then return end
         Notify( "[PasswordReset] Reseting password to default one" )
         Server.SetPassword( tostring( self.Config.DefaultPassword ) or "" )
     end )
